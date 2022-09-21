c9c101359f652b770055a4418a7e0387ae66fcfa
EC2 Default User <ec2-user@ip-20-0-0-90.ec2.internal>
2022-09-21 02:16:04 +0000

    commitng some sjava programme

---
+++ calculatorClassDemo.java
@@ -0,0 +1,29 @@
+package samplePkg;
+
+public class calculatorClassDemo {
+	
+	int value1=0,value2=0;
+	float value3=0;
+	
+	// declaration of funciton
+	public int addition(int a,int b){
+		
+		 
+		value1=a+b;
+		return value1;}
+	
+	public float divition(int a,int b){
+		value3=a/b;
+		return value1;}
+	
+	public int multiply(int a,int b){
+		value2=a*b;
+		return value2;}
+	
+	public int substract(int a,int b){
+		
+		value1=a-b;
+		return value1;}
+	
+
+}
